18e9844f9e6c25ce24649290ec34661b796bb6f4
khushig98 <158275810+khushig98@users.noreply.github.com>
2024-01-31 15:50:30 +0530

    Create Home.py

---
+++ Home.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config(page_title='Attendance System', layout='wide')
+
+st.header('Attendance System using Face Recognition ')
+
+with st.spinner("Developed by Harsh Vaidya"):
+    import face_rec
+
+st.success('Welcome to Face Recognition System')
